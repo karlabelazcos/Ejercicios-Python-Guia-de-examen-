facf290281849de1cdf4e1f2afef8c2605337f2a
karlabelazcos <55230442+karlabelazcos@users.noreply.github.com>
2019-09-23 22:44:16 -0500

    Create Ejercicio8. Compara.py

---
+++ Ejercicio8. Compara.py
@@ -0,0 +1,16 @@
+numero1=input("Numero 1:")
+numero2=input("Numero 2:")
+salida="Numeros proporcionados: {} y {}. {}."
+
+if(numero1==numero2):
+
+    print(salida.format(numero1, numero2,"Los numeros son iguales")
+ else:
+          
+          if numero1>numero2:
+
+              print(salida.format(numero1, numero2,"El mayor es el primero"))
+
+          else:
+          
+             print(salida.format(numero1, numero2,"El mayor es el segundo")
