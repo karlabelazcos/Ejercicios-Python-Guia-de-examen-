d074d6d2308c95a2506498a6f3f85d1ef7a70550
karlabelazcos <55230442+karlabelazcos@users.noreply.github.com>
2019-10-03 00:12:13 -0500

    Update Ejercicio9. Acumulado.py

--- Ejercicio9. Acumulado.py
+++ Ejercicio9. Acumulado.py
@@ -1,11 +1,12 @@
 acumulado=int(0)
 numero=str("")
 
-
+#Un bucle while permite repetir la ejecución de un grupo de instrucciones mientras se cumpla una condición
+#(es decir, mientras la condición tenga el valor True).
 while True:
     numero=input("Dame un numero entero: ")
     if numero=="":
-
+#(Break) ,nos permite salir de adentro de un ciclo (tanto sea for como while ) en medio de su ejecución.
         print("Vacio. Salida del programa.")
         break
     else:
