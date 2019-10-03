ea72f56c2853956b67c69e3d9a9e721dfdf5fe6a
karlabelazcos <55230442+karlabelazcos@users.noreply.github.com>
2019-10-02 23:20:29 -0500

    Update Ejercicio2. Converciones.py

--- Ejercicio2. Converciones.py
+++ Ejercicio2. Converciones.py
@@ -1,11 +1,12 @@
+#Para convertir este tipo de dato se utiliza (str) que convierte (x) a una cadena.
 numero="1234"
-
+#La funcion (type) se utiliza para obtener el tipo de dato de cual objeto.
 print(type(numero))
-
+#La cadena se convierte en entero (int).
 numero=int(numero)
 
 print(type(numero))
-
+#Pones un (str) y un entre corchetes para poner los valores propocionados (formant).
 salida="El numero utilizado es {}"
 
 print(salida.format(numero))
