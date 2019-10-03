31813e5028f642d9a6b42300b431e1b180b11b33
karlabelazcos <55230442+karlabelazcos@users.noreply.github.com>
2019-10-02 23:53:45 -0500

    Update Ejercicio6. Tabla.py

--- Ejercicio6. Tabla.py
+++ Ejercicio6. Tabla.py
@@ -1,6 +1,10 @@
 numero=input("Dame un numero del 1 al 9: ")
 numero=int(numero)
 
+#Un bucle (for) es un bucle que repite el bloque de instrucciones un número prederminado de veces.
+#El tipo range es una lista inmutable de números enteros en sucesión aritmética.
+#Inmutable significa que, a diferencia de las listas, los range no se pueden modificar.
+#Una sucesión artimética es una sucesión en la que la diferencia entre dos términos consecutivos es siempre la misma.
 for i in range(1,11):
 
     salida="{} * {} = {}"
