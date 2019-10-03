a40e3e0bca69071885c4e701d4e52a887ef0d85a
karlabelazcos <55230442+karlabelazcos@users.noreply.github.com>
2019-10-02 23:45:35 -0500

    Update Ejercicio5. Nombre.py

--- Ejercicio5. Nombre.py
+++ Ejercicio5. Nombre.py
@@ -1,7 +1,8 @@
+#Se ponen variales tipos (str).
 nombre=input("Nombre:")
 apellidos=input("Apellidos:")
-
+#Pones el signo de + para juntar el nombre y el apellido.
 nombreCompleto=nombre+" "+apellidos
-
+#El (upper) es para convertir una cadena a mayusculas.
 nombreCompleto=nombreCompleto.upper()
 print(nombreCompleto)
