84a01ecd4e1e99a6232bd7328984b405593160bc
karlabelazcos <55230442+karlabelazcos@users.noreply.github.com>
2019-10-02 23:38:14 -0500

    Update Ejercicio4. Entrada.py

--- Ejercicio4. Entrada.py
+++ Ejercicio4. Entrada.py
@@ -1,9 +1,12 @@
+#La función input() permite obtener texto escrito por teclado.
 entrada=input()
 print(type(entrada))
 
 esEntero=(entrada.isdigit() and entrada.find(".")== -1)
 if(esEntero):
-
+#La primera condicion es para comparar si es verdadera.
 print("Dato entero. ¡Muy bien!")
+#El (else) es como un (SiNo) solo una condicion se debe cumplir.
 else:
+    #O si es falsa.
     print("Dato no es entero. Intentar nuevamente.")
