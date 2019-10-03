6ee695c35d4f6bac5fe422b1ec5c76b3122893f7
karlabelazcos <55230442+karlabelazcos@users.noreply.github.com>
2019-10-02 23:32:16 -0500

    Update Ejercicio3. Aleatorio.py

--- Ejercicio3. Aleatorio.py
+++ Ejercicio3. Aleatorio.py
@@ -1,11 +1,13 @@
+#Se utiliza import para elegir un elemento aleatorio de una lista.
 import random
-
+#Los numeros con punto decimal es de tipo (float).
 numero1=float(10.5)
-
+#La funcion es un conjunto de instrucciones.
+#Una definición de función es una sentencia ejecutable.
 def miFuncion():
 
     numero2=float(random.randrange(1,10))
-
+    #Los entre corchetes son para mostrar los resultados.
     mensaje="La suma de {} y {} es {}"
     print(mensaje.format(numero1,numero2,numero1+numero2))
 
